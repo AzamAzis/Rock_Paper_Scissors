2be10cc01d05ea9dfc4ea020e9de5ee8403b1d09
AzamAzis <aziz.76218@gmail.com>
2026-04-13 15:53:57 +0700

    Add UI

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
-#  Rock Paper Scissors
-Rock Paper Scissors is a JavaScript project to test my javascript foundation knowledge.
+# Odin Project - Rock Paper Scissors
+
+This is a solution to the [QR code component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/qr-code-component-iux_sIO_H). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Odin Project - Rock Paper Scissors](#odin-project---rock-paper-scissors)
+  - [Table of contents](#table-of-contents)
+  - [Overview](#overview)
+    - [Screenshot](#screenshot)
+    - [Links](#links)
+  - [My process](#my-process)
+    - [Built with](#built-with)
+    - [What I learned](#what-i-learned)
+      - [1. JavaScript Fundamentals](#1-javascript-fundamentals)
+      - [2. Game Logic](#2-game-logic)
+      - [3. DOM Manipulation](#3-dom-manipulation)
+      - [4. Event Listeners](#4-event-listeners)
+      - [5. Score Tracking](#5-score-tracking)
+      - [6.Basic UI and Styling (CSS)](#6basic-ui-and-styling-css)
+    - [Challenges](#challenges)
+  - [Author](#author)
+  - [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### Screenshot
+
+<figure style="width: 100%; display: flex; align-items: center; flex-direction: column">
+  <img
+    src="img/screenshot.png"
+    width="250px"
+    height="450px"
+    style="display: block; margin-bottom: 1rem; text-align: center" alt="screenshot">
+    <figcaption>Preview</figcation>
+</figure>
+
+### Links
+
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- Flexbox
+- Mobile-first workflow
+- JavaScript
+
+### What I learned
+
+This project is part of The Odin Project curriculum. The goal was to build a simple Rock Paper Scissors game using JavaScript, HTML, and CSS.
+
+#### 1. JavaScript Fundamentals
+
+- Variables (let, const)
+- Data types (strings, numbers)
+- Functions and return values
+- Conditional logic (if, else if, else)
+- The switch statement
+- Comparison operators (===, >, <)
+- Browser events
+
+#### 2. Game Logic
+
+- Comparing player vs computer choices
+- Determining win, lose, or draw
+- Structuring reusable functions
+
+#### 3. DOM Manipulation
+
+- Selecting elements (querySelector)
+- Updating text content
+- Handling button clicks
+
+#### 4. Event Listeners
+
+- Making the game interactive
+- Responding to user input (button clicks)
+
+#### 5. Score Tracking
+
+- Keeping track of player and computer scores
+- Updating UI dynamically
+
+#### 6.Basic UI and Styling (CSS)
+
+- Layout using Flexbox
+- Centering elements
+- Styling buttons and text
+
+### Challenges
+
+- Understanding event flow in JavaScript
+- Making animations (like score increment effects)
+
+## Author
+
+- Website - [Azam Azis](https://github.com/AzamAzis)
+
+## Acknowledgments
+
+Icons used in this project are provided by creators from [Flaticon](https://www.flaticon.com/).
+
+- [rock.png](https://www.flaticon.com/free-icon/granite_6223796?term=stone&page=1&position=3&origin=search&related_id=6223796)
+- [paper.png](https://www.flaticon.com/free-icon/text_3099976?term=paper&page=5&position=83&origin=search&related_id=3099976)
+- [scissors.png](https://www.flaticon.com/free-icon/cut_1151147?term=cut&page=1&position=13&origin=tag&related_id=1151147)
+- [party-popper.png](https://www.flaticon.com/free-icon/party-popper_3146600?term=party&page=1&position=3&origin=search&related_id=3146600)
--- script.js
+++ script.js
@@ -1,61 +1,177 @@
-// ||SCORE
+// ||BATTLE
+const playerChoiceRps = document.querySelector("#player__choice");
+const computerChoiceRps = document.querySelector("#computer__choice");
+const stage = document.querySelector(".stage");
+const battle = document.querySelector(".battle");
+
+const btns = document.querySelectorAll(".btn-choice");
+
+// ||SCORE BOARD
+const scores = document.querySelectorAll(".player__score");
+const yourPoint = document.querySelector("#player-score");
+const computerPoint = document.querySelector("#computer-score");
+const theResult = document.querySelector(".result");
+
+// ||INITIAL SCORE
 let humanScore = 0;
 let computerScore = 0;
+let winner = "";
 
 function playGame() {
-	// ||SELECTION
-	const humanSelection = getHumanChoice();
-	const computerSelection = getComputerChoice();
-
-	// ||COMPUTER'S CHOICE
-	function getComputerChoice() {
-		const rockPaperScissors = "rock, paper, scissors";
-		const rockPaperScissor = rockPaperScissors.split(", ");
-		const choice = rockPaperScissor[Math.floor(Math.random() * rockPaperScissor.length)];
-		return choice;
-	}
-
-	// ||HUMAN'S CHOICE
-	function getHumanChoice() {
-		const choice = prompt('Choose one of these: "Rock", "Paper", or "Scissors"', "")?.toLowerCase();
-		return choice;
-	}
-
-	// ||GAME
+	// ||PLAY ROUND
 	function playRound(humanChoice, computerChoice) {
-		if (
-			(humanChoice === "rock" && computerChoice === "paper") ||
-			(humanChoice === "paper" && computerChoice === "scissors") ||
-			(humanChoice === "scissors" && computerChoice === "rock")
-		) {
-			console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-			humanScore += 0;
-			computerScore += 1;
-		} else if (
-			(humanChoice === "paper" && computerChoice === "rock") ||
-			(humanChoice === "scissors" && computerChoice === "paper") ||
-			(humanChoice === "rock" && computerChoice === "scissors")
-		) {
-			console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-			humanScore += 1;
-			computerScore += 0;
-		} else if (humanChoice === computerChoice) {
-			console.log("it's a tie");
-			humanScore += 0;
-			computerScore += 0;
-		} else {
-			const fail = "You must enter your choice";
-			console.log(fail);
-			return;
-		}
+		if (winner === "") {
+			stage.classList.add("stage-fadeIn");
+			battle.classList.add("battle-initial-one");
 
-		const result = `total of human's score: ${humanScore} and computer's score is ${computerScore}`;
-		console.log(result);
+			// ||COMPUTER SELECTION
+			const computerSelection = getComputerChoice();
+
+			// ||COMPUTER'S CHOICE
+			function getComputerChoice() {
+				const rockPaperScissors = "rock, paper, scissors";
+				const rockPaperScissor = rockPaperScissors.split(", ");
+				const choice =
+					rockPaperScissor[Math.floor(Math.random() * rockPaperScissor.length)];
+				return choice;
+			}
+
+			humanChoice = this.dataset.selection;
+			computerChoice = computerSelection;
+
+			switch (humanChoice) {
+				case "rock":
+					playerChoiceRps.style.background =
+						"url('img/rock.png') no-repeat center / cover";
+					break;
+				case "paper":
+					playerChoiceRps.style.background =
+						"url('img/paper.png') no-repeat center / cover";
+					break;
+				case "scissors":
+					playerChoiceRps.style.background =
+						"url('img/scissors.png') no-repeat center / cover";
+					break;
+			}
+
+			switch (computerChoice) {
+				case "rock":
+					computerChoiceRps.style.background =
+						"url('img/rock.png') no-repeat center / cover";
+					break;
+				case "paper":
+					computerChoiceRps.style.background =
+						"url('img/paper.png') no-repeat center / cover";
+					break;
+				case "scissors":
+					computerChoiceRps.style.background =
+						"url('img/scissors.png') no-repeat center / cover";
+					break;
+			}
+
+			if (
+				(humanChoice === "rock" && computerChoice === "paper") ||
+				(humanChoice === "paper" && computerChoice === "scissors") ||
+				(humanChoice === "scissors" && computerChoice === "rock")
+			) {
+				computerPoint.classList.add("number-effect");
+				humanScore += 0;
+				computerScore += 1;
+			} else if (
+				(humanChoice === "paper" && computerChoice === "rock") ||
+				(humanChoice === "scissors" && computerChoice === "paper") ||
+				(humanChoice === "rock" && computerChoice === "scissors")
+			) {
+				yourPoint.classList.add("number-effect");
+				humanScore += 1;
+				computerScore += 0;
+			} else if (humanChoice === computerChoice) {
+				humanScore += 0;
+				computerScore += 0;
+
+				for (const score of scores) {
+					score.classList.add("number-effect");
+				}
+			} else {
+				const fail = "Result: You must enter your choice";
+				theResult.textContent = fail;
+				return;
+			}
+
+			for (const btn of btns) {
+				btn.addEventListener("animationend", () => {
+					yourPoint.classList.remove("number-effect");
+					computerPoint.classList.remove("number-effect");
+				});
+			}
+
+			// ||TOTAL SCORES
+			yourPoint.textContent = humanScore;
+			computerPoint.textContent = computerScore;
+
+			// ||THE WINNER
+			if (humanScore === 5 || computerScore === 5) {
+				battle.classList.add("battle-fadeOut");
+				theResult.classList.remove("result-fadeOut");
+				theResult.classList.add("result-fadeIn");
+
+				for (const btn of btns) {
+					btn.classList.add("disabled");
+					btn.classList.remove("btn--active", "btn--hover");
+					btn.style.cursor = "not-allowed";
+				}
+			}
+
+			if (humanScore === 5) {
+				winner = "The winner is you";
+				theResult.textContent = winner;
+				theResult.style.color = "#80FF72";
+			} else if (computerScore === 5) {
+				winner = "The winner is your computer";
+				theResult.textContent = winner;
+				theResult.style.color = "#FC2F00";
+			}
+		}
 	}
 
-	playRound(humanSelection, computerSelection);
+	for (const btn of btns) {
+		btn.addEventListener("click", playRound);
+	}
+
+	for (const score of scores) {
+		score.addEventListener("animationend", () => {
+			score.classList.remove("number-effect");
+		});
+	}
 }
 
-for (let i = 0; i < 5; i++) {
-	playGame();
-}
+playGame();
+
+// ||RESET
+const reset = document.querySelector(".reset");
+
+reset.addEventListener("click", () => {
+	yourPoint.textContent = 0;
+	computerPoint.textContent = 0;
+	battle.classList.remove("battle-fadeOut", "battle-initial-one");
+	theResult.classList.add("result-fadeOut");
+	battle.classList.add("battle-initial");
+
+	for (const score of scores) {
+		score.classList.add("number-effect");
+
+		score.addEventListener("animationend", () => {
+			score.classList.remove("number-effect");
+		});
+	}
+
+	for (const btn of btns) {
+		btn.style.cursor = "pointer";
+		btn.classList.add("btn--active", "btn--hover");
+		btn.classList.remove("disabled");
+	}
+
+	humanScore = 0;
+	computerScore = 0;
+	winner = "";
+});
